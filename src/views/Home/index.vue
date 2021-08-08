54b4af528f888474ba9a0265edac25176062f262
yuxin <star@yuxindeMacBook-Air.local>
2021-08-08 14:35:29 +0800

    新增vue-router

---
+++ src/views/Home/index.vue
@@ -0,0 +1,29 @@
+<template>
+<div class="home-example">
+  <img alt="Vue logo" src="../../assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+})
+</script>
+
+<style scoped>
+.home-example {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
